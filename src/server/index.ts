c6c26a64c93a807203930ea903e70db4e63531b6
Sai Miduthuri <saiparthasarathy.m@gmail.com>
2025-12-03 01:57:40 -0800

    Add RabbitMQ connection open/close to server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,24 @@
+import amqp from "amqplib";
+
+
 async function main() {
   console.log("Starting Peril server...");
+  const rabbitMQConnection: string = "amqp://guest:guest@localhost:5672/";
+  const connection = await amqp.connect(rabbitMQConnection);
+  console.log("Connection to RabbitMQ was successful.");
+
+  ["SIGINT", "SIGTERM"].forEach((signal) =>
+    process.on(signal, async () => {
+      try {
+        await connection.close();
+        console.log("RabbitMQ connection closed:", signal);
+      } catch (err) {
+        console.error("Error closing RabbitMQ connection:", err);
+      } finally {
+        process.exit(0);
+      }
+    }),
+  );
 }
 
 main().catch((err) => {
