dcf43ea874dddf08df4974d7a3fb62eeccecf568
Sai Miduthuri <saiparthasarathy.m@gmail.com>
2025-12-09 00:53:43 -0800

    Add msgpack message publish

--- src/internal/pubsub/publish.ts
+++ src/internal/pubsub/publish.ts
@@ -1,4 +1,5 @@
 import type { ConfirmChannel } from "amqplib";
+import { encode } from "@msgpack/msgpack";
 
 export async function publishJSON<T>(
   ch: ConfirmChannel,
@@ -9,4 +10,12 @@ export async function publishJSON<T>(
   const stringifiedValue = JSON.stringify(value);
   ch.publish(exchange, routingKey, Buffer.from(stringifiedValue), { contentType: "application/json" });
   console.log("Published:", stringifiedValue);
+}
+
+export async function publishMsgPack<T>(
+  ch: ConfirmChannel, exchange: string, routingKey: string, value: T
+): Promise<void> {
+  const serializedMessage = encode(value);
+  ch.publish(exchange, routingKey, Buffer.from(serializedMessage), { contentType: "application/x-msgpack" });
+  console.log(`Published: ${value}`);
 }
